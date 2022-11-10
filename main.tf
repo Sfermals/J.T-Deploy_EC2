44436fa1e00c4c1ac3c3295553f9d707d348a398
Sfermals <100915075+Sfermals@users.noreply.github.com>
2022-11-11 02:52:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@ resource "aws_instance" "webserver" {
   ami           = "ami-07651f0c4c315a529"
   instance_type = "t2.micro"
   key_name      = "sf-pem-1"
-  
-  subnet id     = "subnet-0f2c65af3727dafba"
 
   tags = {
     Name = "sf-ec2"
